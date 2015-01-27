ed829d5533cd8932903aee6360c2fe9c18a85d52
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-01-27 15:55:50 +0300

    Used RESTFUL route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-
-  get 'articles/index'
-
-  get 'articles/new'
-
-  get 'articles/create'
-
-  get 'articles/show'
-
-  get 'articles/edit'
-
-  get 'articles/destroy'
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
