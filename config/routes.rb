dde8c4715cf743e427fe001fc30c3135498809c7
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-01-27 15:41:01 +0300

    Add articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'articles/index'
+
+  get 'articles/new'
+
+  get 'articles/create'
+
+  get 'articles/show'
+
+  get 'articles/edit'
+
+  get 'articles/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
