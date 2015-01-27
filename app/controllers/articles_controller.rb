6f093c2d1c5e84037526cf0d740472d307893d26
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-01-27 22:46:57 +0300

    Deleted unneeded files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,2 @@
 class ArticlesController < ApplicationController
-
-  def index
-    @articles = Article.all
-  end
-
-  def new
-    @article = Article.new(article_params)
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  private
-
-    def article_params
-      params.require(:article).permit(:title, :author, :content)
-    end
-
 end
