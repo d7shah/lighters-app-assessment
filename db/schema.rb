5036d7e0539323550aae622e80eef2f844cc8be7
Dhruti Shah <dhrutishah10@gmail.com>
2015-02-02 11:05:20 +0300

    lighters app

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127195422) do
+ActiveRecord::Schema.define(version: 20150127110110) do
 
   create_table "articles", force: :cascade do |t|
+    t.string   "author"
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
