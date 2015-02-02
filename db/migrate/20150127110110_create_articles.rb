a6bcde05119bce3ae9799769338ec5d84d2bb83b
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-02-02 07:29:22 +0300

    Removed everything necessary to make it crazy for them :)

--- db/migrate/20150127110110_create_articles.rb
+++ db/migrate/20150127110110_create_articles.rb
@@ -1,9 +1,7 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string :title
       t.string :author
-      t.text :content
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127110110) do
+ActiveRecord::Schema.define(version: 20150127195422) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "author"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
